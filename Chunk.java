08c342a8020914c4877cb7c633e9632994375e43
Esti Sendrovitz <sendy613@gmail.com>
2015-05-13 16:05:49 -0400

    deleted readme

--- Chunk.java
+++ Chunk.java
@@ -1,4 +1,4 @@
-package dropbox;
+package sendrovitz.dropbox;
 
 public class Chunk {
 	
--- FileCache.java
+++ FileCache.java
@@ -1,4 +1,4 @@
-package dropbox;
+package sendrovitz.dropbox;
 
 import java.io.File;
 import java.util.List;
